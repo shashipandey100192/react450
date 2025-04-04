49bb5c7a6331c6cb97e08c2acd143075d56856d4
shashiweb <shashiweb64@gmail.com>
2025-04-04 16:47:44 +0530

    create new project

---
+++ src/About.js
@@ -0,0 +1,19 @@
+function Myabout()
+{
+    return <div>
+    <h1>this is my about page ofjks dghfsgdfgsdfg</h1> 
+          <h2>sdfhjsdhf</h2>
+          <h1>this is heading</h1>
+    </div>
+}
+export default Myabout
+
+export function About1()
+{
+    return(
+        <div>
+            <h1 style={{color:'red',backgroundColor:'orange'}}>this is my page</h1>
+            <p className="first">this is external css link</p>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./style.css";
+import Abc from './About';
+import { About1 } from './About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>welcome to react</h1>
+    <input/>
+    <h6>sdfshdgf</h6>
+    <Abc></Abc>
+    <About1></About1>
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
