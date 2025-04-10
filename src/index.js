dea9c7c7cbb6b91c95e0dac5dbb4e2e2acd781fd
shashiweb <shashiweb64@gmail.com>
2025-04-10 16:51:41 +0530

    apps

--- src/index.js
+++ src/index.js
@@ -2,26 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-import "./style.css";
-import Abc from './About';
-import { About1 } from './About';
-import Mybootstrappage from './Mybootstrappage';
-import Myreactbootstrap from './Myreactbootstrap';
-import Muipage from './Muipage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>welcome to react</h1>
-    <input/>
-    <h6>sdfshdgf</h6>
-    <Abc></Abc>
-    <About1></About1>
-    <Mybootstrappage></Mybootstrappage>
-    <h1>react bootstrap</h1>
-    <Myreactbootstrap></Myreactbootstrap>
-    <h1>MUI components</h1>
-    <Muipage></Muipage>
+ 
 
   </React.StrictMode>
 );
