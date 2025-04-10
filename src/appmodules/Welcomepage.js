ce38aae62ee29f2b2e7d08a78bdbb4a38b31524e
shashiweb <shashiweb64@gmail.com>
2025-04-10 17:16:36 +0530

    apps

---
+++ src/appmodules/Welcomepage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FaUserSecret } from "react-icons/fa";
+
+function Welcomepage() {
+  return (
+<div class="container py-5">
+  <h2 class="text-center mb-4">Select Account Type to Login</h2>
+  <div class="row justify-content-center">
+
+
+    <div class="col-md-3">
+      <div class="card login-card">
+        <a href='#' class="card-body text-center">
+           <h1><FaUserSecret/></h1>
+          <h5 class="card-title">User Login</h5>
+        </a>
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <div class="card login-card ">
+        <a href='#' class="card-body text-center">
+           <h1><FaUserSecret/></h1>
+          <h5 class="card-title">Admin Login</h5>
+        </a>
+      </div>
+    </div>
+
+  </div>
+</div>
+  )
+}
+
+export default Welcomepage
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import "./appmodules/css/global.css";
+import Welcomepage from './appmodules/Welcomepage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
- 
+    <Welcomepage/>
 
   </React.StrictMode>
 );
